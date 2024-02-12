cafadae3db1c0665db0271a3377e0c72f3eb2e59
AthkiaAziza <athkiaaziza@gmail.com>
2024-02-12 23:31:14 +0600

    added rectangle

--- scripts/rectangle-calculate.js
+++ scripts/rectangle-calculate.js
@@ -3,5 +3,28 @@
 
 
 function calculateRectangleArea() {
-    console.log('calculate rect area');
+    //console.log('calculate rect area');
+
+    // get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    //console.log(widthInput);
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+    //console.log(widthText);
+
+
+    // get length of the rectangle
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText)
+    console.log(length);
+
+    // calculate rectangle area
+    const area = width * length;
+    console.log('Area of the rectangle:', area);
+
+    //display triangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
